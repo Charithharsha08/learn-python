98571e9b39561d721402ebfe55b61cf94af73b77
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-07-24 13:04:24 +0530

    day 8

---
+++ debugger/python_debugger.py
@@ -0,0 +1,13 @@
+import pdb
+def multification(a, b):
+    return a * b
+
+# set breakpoint
+#pdb.set_trace()
+
+breakpoint()
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+
+mul = multification(x, y)
+print(mul)
